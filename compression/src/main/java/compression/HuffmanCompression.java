d9ffba93ba9b9969f918aa88ea58ae9ee7734efb
TuomoM <tuomo.mehtala@gmail.com>
2021-11-11 19:21:57 +0200

    Hoffman coding first version

---
+++ compression/src/main/java/compression/HuffmanCompression.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package compression;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+/**
+ *
+ * @author tuomomehtala
+ */
+public class HuffmanCompression {
+    private HashMap keys;
+    private HuffmanNode nodes;
+    private HashMap<Character,Integer> occurences;
+    private String data;
+    private String dataPacked;
+    
+    public HuffmanCompression(String data){
+       this.data = data;
+        
+        HashMap<Character,Integer> occurences = new HashMap<>();
+        
+        for(int i = 0 ; i < data.length();i++){
+            occurences.put(data.charAt(i), occurences.getOrDefault(data.charAt(i), 0)+1);
+        }
+        this.occurences = occurences;
+        generateNodes(occurences);
+        generateKeys(this.nodes,"");
+        generatePacked();
+    }
+    public HuffmanCompression(String packedData,HashMap<Character,Integer> occurences){
+        this.dataPacked = packedData;
+        generateNodes(occurences);
+        generateData(0);
+    }
+    private void generateData(int index){
+        HuffmanNode node = this.nodes;
+        while(!node.isLeaf()){
+            if(this.dataPacked.charAt(index)==0){
+                node = node.getLeft();
+            }else{
+                node = node.getRight();
+            }
+            index++;
+                
+        }
+        this.data = this.data+node.getLeaf();
+        if(index<this.dataPacked.length()){
+            generateData(index);
+        }
+    }
+    private void generateNodes(HashMap<Character,Integer> occurences){
+        HuffmanNode node1,node2;
+        PriorityQueue<HuffmanNode> queue = new PriorityQueue<>();
+        for(char a : occurences.keySet()){
+            queue.add(new HuffmanNode(a, occurences.get(a)));
+            
+        }
+        while(!queue.isEmpty()){
+            node1 = queue.poll();
+            if(queue.isEmpty()){
+                nodes = node1;
+            }else{
+                node2 = queue.poll();
+                queue.add(new HuffmanNode(node1,node2));
+            }
+            
+        }
+    }
+    private void generateKeys(HuffmanNode node,String key){
+        if(node.isLeaf()){
+            this.keys.put(node.getLeaf(), key);
+            return;
+        }
+        generateKeys(node.getLeft(),key+0);
+        generateKeys(node.getRight(),key+1);
+    }
+    private void generatePacked(){
+        for(int i = 0; i<this.data.length();i++){
+            char a = data.charAt(i);
+            dataPacked = dataPacked+a;
+        }
+    }
+    public String getPackedData(){
+        return this.dataPacked;
+    }
+    public String getData(){
+        return this.data;
+    }
+    public HashMap<Character,Integer> getOccurences(){
+        return this.occurences;
+    }
+    
+}
---
+++ compression/src/main/java/compression/HuffmanNode.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compression;
+
+/**
+ *
+ * @author tuomomehtala
+ */
+public class HuffmanNode implements Comparable {
+    private HuffmanNode left;
+    private HuffmanNode right;
+    private int value;
+    private char leaf;
+    private boolean isLeaf;
+    
+    public HuffmanNode(HuffmanNode left, HuffmanNode right){
+        this.left = left;
+        this.right = right;
+        this.value = right.getValue() + left.getValue();
+        this.isLeaf = false;
+    }
+    public HuffmanNode(char leaf, int value){
+        this.value = value;
+        this.leaf = leaf;
+        this.isLeaf = true;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public boolean isLeaf(){
+        return this.isLeaf;
+    }
+    @Override
+    public int compareTo(Object o) {
+        if(o instanceof HuffmanNode){
+            HuffmanNode node = (HuffmanNode)o;
+            if (this.value > node.getValue()) return 1;
+            if(this.value < node.getValue()) return -1;
+            return 0;
+        }
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
